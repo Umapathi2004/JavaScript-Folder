83b85b43e88e10c109575c46d65f55c6a14bfd76
Umapathi <umapathi1014@gmail.com>
2024-05-29 20:07:10 +0530

    Create climbStairs.js

---
+++ climbStairs.js
@@ -0,0 +1,20 @@
+function climbStairs(n) {
+    if (n === 1) {
+        return 1;
+    }
+    if (n === 2) {
+        return 2;
+    }
+
+    let first = 1;
+    let second = 2;
+
+    for (let i = 3; i <= n; i++) {
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return second;
+}
+console.log(climbStairs(2))
