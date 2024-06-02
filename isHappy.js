903fee81f0f0bad6c9ed7870a4984a073c4df5a8
Umapathi <umapathi1014@gmail.com>
2024-06-02 19:14:15 +0530

    Create isHappy.js

---
+++ isHappy.js
@@ -0,0 +1,19 @@
+function isHappy(n){
+    if(n<=0){
+        return false;
+    }
+  let temp=(n+"").split("");
+  let added=0,count=0;
+  while(true){
+    for(let num of temp){
+        added+=(num**2);
+    }
+    if(added==1){
+        return true;
+    }
+    temp=(added+"").split("");
+    added=0;
+    count++;
+  }
+}
+console.log(isHappy(2))
